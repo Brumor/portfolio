f0aeac9cce0e13e32cf03bd29c656a4a81df4eb1
Brumor <paulbricout2@gmail.com>
2020-05-22 21:20:51 +0200

    Updates

---
+++ precache-manifest.cc214750cf6a94cfd2a9cbfbd0e370e1.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c8d2814e544545027294b6da9d15d2f",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "0b74c4968e61a7557d90",
+    "url": "/portfolio/static/css/main.29dd0c4a.chunk.css"
+  },
+  {
+    "revision": "f8d4fa7c06d3ec313002",
+    "url": "/portfolio/static/js/2.ec967a86.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.ec967a86.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b74c4968e61a7557d90",
+    "url": "/portfolio/static/js/main.830dd8b1.chunk.js"
+  },
+  {
+    "revision": "88fbd3d397ae3690d5f2",
+    "url": "/portfolio/static/js/runtime-main.fb1b3dac.js"
+  },
+  {
+    "revision": "846ec0fa27001a923cc04d072bc6441c",
+    "url": "/portfolio/static/media/Paul_BRICOUT_resume.846ec0fa.pdf"
+  },
+  {
+    "revision": "43b3f7cfed5ae135ffe72058538c9a99",
+    "url": "/portfolio/static/media/france_640.43b3f7cf.png"
+  },
+  {
+    "revision": "2c6fe6d83c747fc6e2b33aed44afed32",
+    "url": "/portfolio/static/media/germany_640.2c6fe6d8.png"
+  },
+  {
+    "revision": "29176c5c0759f113580651ca780bd86c",
+    "url": "/portfolio/static/media/italy_640.29176c5c.png"
+  },
+  {
+    "revision": "d2ffd85ec17d12800b1b9138fa33233b",
+    "url": "/portfolio/static/media/myAvatar.d2ffd85e.png"
+  },
+  {
+    "revision": "cd382d68094a28a93a7f7581739873d4",
+    "url": "/portfolio/static/media/russia_640.cd382d68.png"
+  },
+  {
+    "revision": "d35eca178d3e9a4fce7e759ef7fb25e3",
+    "url": "/portfolio/static/media/sweden_640.d35eca17.png"
+  },
+  {
+    "revision": "7a9d4c10f7ff80c3303e03a361df0b39",
+    "url": "/portfolio/static/media/united_kingdom_640.7a9d4c10.png"
+  }
+]);
